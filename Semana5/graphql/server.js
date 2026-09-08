d58556040f9f5b9a633f8f03ab23c6590b07ab97
Franco González <franco.gonzalez.orellana@gmail.com>
2026-09-07 21:37:35 -0300

    Add files via upload

--- Semana5/graphql/server.js
+++ Semana5/graphql/server.js
@@ -1,114 +1,83 @@
-// Cuando se llama sin "{}" se importa el objeto o función completa que exporta el módulo
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors")
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-// Cuando se usan "{}" (destructuración), se extraen solo funciones o propiedades específicas de ese módulo
-const {ApolloServer, gql} = require("apollo-server-express");
+const {ApolloServer, gql} = require('apollo-server-express');
+const Usuario = require('./models/usuario');
 
-const Juego = require("./models/juegos");
-
-mongoose.connect("mongodb://localhost:27017/Videojuegos");
-// Para concetar con la bd atravez de un modelo este require 3 cosas
+mongoose.connect('mongodb://localhost:27017/bdunab2');
 const typeDefs = gql`
-    type Juego{
+    type Usuario{
         id: ID!
         nombre: String!
-        desarollador: String!
-        editor: String!
-        fecha: String!
-        precio: Float!
-        claveAct: String!
+        pass: String!
     }
-    input JuegoInput{
+    input UsuarioInput{
         nombre: String!
-        desarollador: String
-        editor: String
-        fecha: String!
-        precio: Float!
-        claveAct: String!
+        pass: String!
     }
-    input DevInput {
-        desarrollador: String!
-    }
-    type Alert {
+    type Alert{
         message: String
     }
     type Query{
-        getJuegos: [Juego]
-        getJuegoById(id: ID!): Juego
-        getJuegoByDev(desarollador: String!): [Juego]
+        getUsuarios: [Usuario]
+        getUsuariosById(id: ID!): Usuario
     }
     type Mutation{
-        addJuego(input: JuegoInput) : Juego
-        updJuego(id: ID!, input: JuegoInput): Juego
-        delJuego(id: ID!): Alert
-    }`
-;
+        addUsuario(input: UsuarioInput): Usuario
+        updUsuario(id: ID!, input: UsuarioInput): Usuario
+        delUsuario(id: ID!): Alert
+    }
+`;
 
 const resolvers = {
-    // Se escriben las funciones a utilizar en gql
     Query: {
-        // Funcion asicrona no espera una respuesta, pero no significa dentro de esta llamada no existan cosas que esperan respuestas
-        async getJuegos(obj){
-            // Espera para evaluar que encuentre el valor indicado
-            const juegos = await Juego.find()
-            return juegos;
+        async getUsuarios(obj){
+            const usuarios = await Usuario.find();
+            return usuarios;
         },
-        async getJuegoById(obj,{id}){
-            const juegoBuscado = await Juego.findById(id);
-            if (juegoBuscado == null){
+        async getUsuariosById(obj, {id}){
+            const usuarioBus = await Usuario.findById(id);
+            if (usuarioBus == null){
                 return null;
             } else {
-                return juegoBuscado;
+                return usuarioBus;
             }
-        },
-        async getJuegoByDev(obj,{dev}){
-            const juegoBuscado = await Juego.find({ desarollador: dev });
-            return juegoBuscado
         }
     },
     Mutation: {
-        async addJuego(obj,{input}){
-            const juego = new Juego(input);
-            await juego.save();
-            return juego;
+        async addUsuario(obj, {input}){
+            const usuario = new Usuario(input);
+            await usuario.save();
+            return usuario;
         },
-        async updJuego(obj,{id,input}){
-            const juego = await Juego.findByIdAndUpdate(id,input);
-            return juego;
+        async updUsuario(obj, {id, input}){
+            const usuario = await Usuario.findByIdAndUpdate(id, input);
+            return usuario;
         },
-        async delJuego(obj,{id}){
-            const juegoBorrado = await Juego.findByIdAndDelete(id);
-            if (!juegoBorrado){
-                return {
-                    message:"Error: El juego no existe o ya fue eliminado"
-                };
-            }
+        async delUsuario(obj, {id}){
+            await Usuario.deleteOne({_id: id});
             return {
-                message: `Juego ${juegoBorrado.nombre} eliminado`
-            };
-        },
+                message: "Usuario Eliminado"
+            }
+        }
     }
 };
 
 let apolloServer = null;
 const corsOption = {
-    origin: "https://localhost:8090",
+    origin: "http://localhost:8090",
     credentials: false
 };
-
-const app = express();
-app.use(cors());
-
 async function startServer(){
-    apolloServer = new ApolloServer({typeDefs, resolvers,corsOption});
+    apolloServer = new ApolloServer({typeDefs, resolvers, corsOption});
     await apolloServer.start();
     apolloServer.applyMiddleware({app, cors: false});
 }
-
 startServer();
 
+const app = express();
+app.use(cors());
 app.listen(8090, function(){
-    console.log("Graphql Iniciado en http://localhost:8090/graphql");
+    console.log("Graphql Iniciado");
 });
